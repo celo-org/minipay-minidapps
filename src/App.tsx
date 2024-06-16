8bc9ed0455e677c03b8ca9f1a81838dcd1c62dc8
samkeleN <samkelendzululeka@gmail.com>
2024-06-16 20:59:03 +0200

    chore: Update react-scripts to latest version

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ export default function App() {
     return (
         <div
             style={{
-                marginTop: "20px",
+                marginTop: "30px",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
                 gap: "12px",
+                backgroundColor: "lightgray",
+                padding: "20px",
+                borderRadius: "80px",
             }}
         >
             <TransferCUSD />
-            <PayTokenFunctionCall />
         </div>
     );
 }
--- src/dApps/TransferCUSD.tsx
+++ src/dApps/TransferCUSD.tsx
@@ -1,48 +1,78 @@
-import { utils } from "ethers";
+import { ethers } from "ethers";
+import React, { useState } from "react";
 
 // Mainnet address of cUSD
 const CUSD_ADDRESS = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 
-const receiverAddress = "";
-
 // DApp to quickly test transfer of cUSD to a specific address using the cUSD contract.
 export default function TransferCUSD() {
+    const [userAddress, setUserAddress] = useState<string>("");
+    const [receiverAddress, setReceiverAddress] = useState<string>("");
+    const [transactionStatus, setTransactionStatus] = useState<string>("");
+
     async function transferCUSD() {
-        if (window.ethereum) {
-            // Get connected accounts, if not connected request connnection.
-            // returns an array of accounts
-            let accounts = await window.ethereum.request({
+        try {
+            const ethereum = window.ethereum;
+            if (!ethereum) {
+                throw new Error("MetaMask not detected. Please install MetaMask.");
+            }
+
+            const accounts = await ethereum.request({
                 method: "eth_requestAccounts",
             });
+            setUserAddress(accounts[0]);
 
-            // The current selected account out of the connected accounts.
-            let userAddress = accounts[0];
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
 
-            let iface = new utils.Interface([
-                "function transfer(address to, uint256 value)",
-            ]);
+            const cUSDContract = new ethers.Contract(
+                CUSD_ADDRESS,
+                ["function transfer(address to, uint256 value)"],
+                signer
+            );
 
-            let calldata = iface.encodeFunctionData("transfer", [
-                receiverAddress,
-                utils.parseEther("0.1"), // 10 cUSD - This amount is in wei
-            ]);
+            const tx = await cUSDContract.transfer(receiverAddress, ethers.utils.parseEther("0.1"));
+            await provider.waitForTransaction(tx.hash);
 
-            // Send transaction to the injected wallet to be confirmed by the user.
-            let tx = await window.ethereum.request({
-                method: "eth_sendTransaction",
-                params: [
-                    {
-                        from: userAddress,
-                        to: CUSD_ADDRESS, // We need to call the transfer function on the cUSD token contract
-                        data: calldata, // Information about which function to call and what values to pass as parameters
-                    },
-                ],
-            });
-
-            // Wait until tx confirmation and get tx receipt
-            let receipt = await tx.wait();
+            setTransactionStatus(`Transaction successful!!`);
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error("Error transferring cUSD:", error);
+                setTransactionStatus(`Error: ${error.message}`);
+            }
         }
     }
 
-    return <button onClick={transferCUSD}>Transfer cUSD</button>;
+    return (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            {userAddress && <p>Your Address: {userAddress}</p>}
+            <input
+                type="text"
+                value={receiverAddress}
+                onChange={(e) => setReceiverAddress(e.target.value)}
+                placeholder="Receiver's Address"
+                style={{ 
+                    marginBottom: "10px", 
+                    borderRadius: "10px",
+                    padding: "5px",
+                    width: "200px",
+                    border: "1px solid grey",
+                }}
+            />
+            <button 
+                className="transfer" 
+                onClick={transferCUSD} 
+                style={{ 
+                    marginBottom: "10px", 
+                    borderRadius: "10px",
+                    padding: "5px", 
+                    border: "1px solid green",
+                    color: "green",
+                }}
+            >
+                Transfer cUSD
+            </button>
+            {transactionStatus && <p>{transactionStatus}</p>}
+        </div>
+    );
 }
